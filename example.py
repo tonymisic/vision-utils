dc326bb72c60bb05db52a25fbd34da4100e957e6
Tony Misic <tony.misic98@gmail.com>
2021-07-06 20:20:45 -0400

    initialization and preliminary work

---
+++ example.py
@@ -0,0 +1,11 @@
+'''
+Examples of using code
+'''
+import torch
+from visualization import Visualization
+
+sample_video = torch.rand(10, 512, 7, 7)
+sample_audio = torch.rand(10, 128)
+temporal_label = 
+spatial_label = 
+Visualization.tsne()
